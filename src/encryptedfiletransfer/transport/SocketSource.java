b140b651be98f3010a210ebd02cfd3850f8a9ab6
George Antonious <george@antonious.ca>
2017-04-09 15:39:21 -0600

    Abstract socket read and writes

--- src/encryptedfiletransfer/transport/SocketSource.java
+++ src/encryptedfiletransfer/transport/SocketSource.java
@@ -1,18 +1,46 @@
 package encryptedfiletransfer.transport;
 
 import encryptedfiletransfer.InputOutputSource;
+import encryptedfiletransfer.transport.exceptions.SocketIOException;
+
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * Created by George on 2017-04-09.
  */
 public class SocketSource implements InputOutputSource {
+    private Socket socket;
+
+    public SocketSource(Socket socket) {
+        this.socket = socket;
+    }
+
     @Override
     public byte[] read(int total) {
-        return new byte[0];
+        try {
+            return readFrom(socket, total);
+        } catch (IOException e) {
+            throw new SocketIOException(e.getMessage());
+        }
     }
 
     @Override
     public void write(byte[] data) {
+        try {
+            writeTo(socket, data);
+        } catch (IOException e) {
+            throw new SocketIOException(e.getMessage());
+        }
+    }
 
+    private byte[] readFrom(Socket socket, int total) throws IOException {
+        byte[] output = new byte[total];
+        socket.getInputStream().read(output);
+        return output;
+    }
+
+    private void writeTo(Socket socket, byte[] data) throws IOException {
+        socket.getOutputStream().write(data);
     }
 }
---
+++ src/encryptedfiletransfer/transport/exceptions/SocketIOException.java
@@ -0,0 +1,10 @@
+package encryptedfiletransfer.transport.exceptions;
+
+/**
+ * Created by George on 2017-04-09.
+ */
+public class SocketIOException extends RuntimeException {
+    public SocketIOException(String message) {
+        super(message);
+    }
+}
