50398d9e95c6af68241576cf9c9c2a9bdbd395b7
George Antonious <george@antonious.ca>
2017-04-09 16:19:59 -0600

    Add FileRequest message

---
+++ src/filetransfer/protocol/messages/FileRequest.java
@@ -0,0 +1,18 @@
+package filetransfer.protocol.messages;
+
+/**
+ * Created by George on 2017-04-09.
+ */
+public class FileRequest extends Message {
+    public FileRequest(String fileName) {
+        super(MessageTypes.FILE_REQUEST, fileName);
+    }
+
+    public String getFileName() {
+        return getData();
+    }
+
+    public static FileRequest from(String data) {
+        return new FileRequest(data);
+    }
+}
