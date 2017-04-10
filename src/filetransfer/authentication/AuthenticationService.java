df8d5c229d654504eb504d0dd97687c2fd0108f2
George Antonious <george@antonious.ca>
2017-04-09 18:21:08 -0600

    Add authentication service

---
+++ src/filetransfer/authentication/AuthenticationService.java
@@ -0,0 +1,10 @@
+package filetransfer.authentication;
+
+/**
+ * Created by George on 2017-04-09.
+ */
+public class AuthenticationService {
+    public boolean isUserAuthenticated(String username, String password) {
+        return true;
+    }
+}
