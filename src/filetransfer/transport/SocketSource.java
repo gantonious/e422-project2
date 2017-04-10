681921b9c9c2e9108e41b7680ebdb91d72ed4ae8
George Antonious <george@antonious.ca>
2017-04-09 20:32:40 -0600

    ALL HAIL THE DATAINPUTSTREAM

--- src/filetransfer/transport/SocketSource.java
+++ src/filetransfer/transport/SocketSource.java
@@ -3,9 +3,7 @@ package filetransfer.transport;
 import filetransfer.InputOutputSource;
 import filetransfer.shared.exceptions.SocketIOException;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 
 /**
@@ -13,15 +11,15 @@ import java.net.Socket;
  */
 public class SocketSource implements InputOutputSource {
     private Socket socket;
-    private BufferedInputStream bufferedInputStream;
-    private BufferedOutputStream bufferedOutputStream;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
 
     public SocketSource(Socket socket) {
         this.socket = socket;
 
         try {
-            bufferedInputStream = new BufferedInputStream(socket.getInputStream());
-            bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
         } catch (Exception e) {
             throw new SocketIOException(e.getMessage());
         }
@@ -54,27 +52,14 @@ public class SocketSource implements InputOutputSource {
         }
     }
 
-    // TODO: maybe refactor if time permits
     private byte[] readFromBuffer(int total) throws IOException {
         byte[] output = new byte[total];
-
-        int amountRead = bufferedInputStream.read(output, 0, total);
-        int totalRead = amountRead;
-        int totalToRead = total - amountRead;
-
-        while (amountRead > 0 && totalRead != total) {
-            amountRead = bufferedInputStream.read(output, totalRead, totalToRead);
-            if (amountRead > 0) {
-                totalRead += amountRead;
-                totalToRead -= amountRead;
-            }
-        }
+        dataInputStream.readFully(output);
 
         return output;
     }
 
     private void writeToBuffer(byte[] data) throws IOException {
-        bufferedOutputStream.write(data);
-        bufferedOutputStream.flush();
+        dataOutputStream.write(data);
     }
 }
